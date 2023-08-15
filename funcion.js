5eb0768fb37293052b14f6977ee12df8ac2a725a
XAl3j0 <142344942+XAl3j0@users.noreply.github.com>
2023-08-15 13:33:09 -0500

    Add files via upload

---
+++ funcion.js
@@ -0,0 +1,32 @@
+
+function mostrar(){
+    document.getElementById('MostrarDiv').style.display = 'block'
+}
+
+function ocultar(){
+    document.getElementById('quitarLinks').style.display = 'none'
+    return;
+}
+
+function mostarUL(){
+    document.getElementById('quitarLinks').style.display = 'flex'
+}
+function ocultarInput(){
+    document.getElementById('MostrarDiv').style.display = 'none'
+
+}
+
+
+function quitarLupa(){
+    document.getElementById('lupaMostrar').style.display ='none'
+}
+function mostrarX(){
+    document.getElementById('Xprincipal').style.display = 'block'
+}
+function regresoLupa(){
+    document.getElementById('lupaMostrar').style.display ='block'
+}
+
+function borrarX(){
+    document.getElementById('Xprincipal').style.display = 'none'
+}
